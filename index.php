fc9099517f712348fac6ea0dbc08e6586f4989ee
gusgusteam <lucasgustavocarvajalbarrios@gmail.com>
2023-01-09 16:33:01 -0800

    lucas
    primer commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Presentacion</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container">
+    <div class="mb-3">
+        <div class="text text-center">
+            <h3>Elija una opcion</h3>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="text text-center">
+                <form action="opcion1.php" method="post" class="form_contact">
+                    <input type="submit" value="Correo" id="btnSend">
+                </form>
+                <br>
+                <form action="opcion2.php" method="post" class="form_contact">
+                    <input type="submit" value="Proyecto" id="btnSend">
+                </form>
+            </div>
+            
+        </div>
+
+    </div>
+</div>
+
+    
+</body>
+</html>
---
+++ opcion1.php
@@ -0,0 +1,5 @@
+<?php  
+
+header('Location:correo_laravel_8/public');
+
+?>
